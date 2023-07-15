0c5121a9037982723327f8c3165b7ae5bb5add53
Harshda Mhase <harshadamhase12@gmail.com>
2023-07-14 21:46:57 -0900

    ref: url-project-completed

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', async(req,res) => {
-   const shortUrls = await ShortUrl.find()
+   const shortUrls = await ShortUrls.find()
  res.render('index', {shortUrls: shortUrls})   
 })
 
@@ -29,6 +29,17 @@ app.post('/shortUrls',async (req,res)=>{
  await ShortUrls.create({ full: req.body.fullUrl})
 
  res.redirect('/')
+})
+
+app.get('/:shortUrl',async (req,res)=>{
+
+  const shortUrl = await ShortUrls.findOne({ short: req.params.shortUrl })
+  if(shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+  
+  res.redirect(shortUrl.full)
 })
 
 const PORT = process.env.PORT || 5000
